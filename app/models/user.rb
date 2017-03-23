005047b2b9e5af8b526344e0ba3b4bc76872a110
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-03-23 20:33:58 +0100

    Auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates_confirmation_of :password, on: :create
   validates_uniqueness_of :email
+  validates :auth_token, uniqueness: true
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
