181301a8f4cb40c484e35f960a1a038f607857fc
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-03-29 01:22:31 +0200

    Adds destroy session action added

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -36,4 +36,16 @@ describe Api::V1::SessionsController do
     end
   end
 
+  describe "DELETE #destroy" do
+    let(:user) {FactoryGirl.create :user}
+
+    before(:each) do
+      sign_in user
+      delete :destroy, id: user.auth_token
+    end
+
+    it { should respond_with 204 }
+
+  end
+
 end
