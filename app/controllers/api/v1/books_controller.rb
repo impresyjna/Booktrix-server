78c1695496ddd949752a3dbfce0f0f25e1ec3fff
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-04-07 00:02:48 +0200

    Added google api to project

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::BooksController < ApplicationController
+  respond_to :json
+
+  def found_index
+
+  end
+
+  def found_by_isbn
+    book_service = Google::Apis::BooksV1::BooksService
+    book = book_service.volumes.get()
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :publisher, :isbn, :image, :description, :publish_date, :page_count)
+  end
 end
