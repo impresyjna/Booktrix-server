1138ad8621e4525235158c27e37e0e5d629c2347
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-04-09 18:28:27 +0200

    Revert "Added google api to project"
    This reverts commit 78c1695496ddd949752a3dbfce0f0f25e1ec3fff.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
     scope module: :v1 do
       resources :users, :only => [:show, :create, :update, :destroy]
       resources :sessions, :only => [:create, :destroy]
-      get 'found_by_isbn' => 'books#found_by_isbn'
-      get 'found_index' => 'books#found_index'
     end
   end
 end
--- spec/controllers/api/v1/books_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Api::V1::BooksController, type: :controller do
-
-end
