9914f535772a09dd1ffa727fa46d784088604973
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-04-09 18:28:52 +0200

    Revert "Revert "Book model and gem for google api I hope""
    This reverts commit f380260a97f6ed83f7131354ac243d461b5c3a08.

---
+++ spec/factories/books.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :book do
+    title "MyString"
+    author "MyString"
+    publisher "MyString"
+    isbn "MyString"
+    image "MyString"
+    description "MyText"
+    publish_date "2017-04-06 22:00:42"
+    page_count ""
+  end
+end
