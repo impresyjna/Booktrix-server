3da758263f00011889e36a38899669fc0e5fd9b0
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-04-19 23:19:55 +0200

    Back to tests

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,30 +1,30 @@
 require 'spec_helper'
 
 RSpec.describe Api::V1::BooksController, type: :controller do
-  # describe "GET #found_by_isbn" do
-  #   context "when is successfully founded" do
-  #     before(:each) do
-  #       @book = FactoryGirl.create :book
-  #       get :found_by_isbn, isbn: @book.isbn
-  #     end
-  #
-  #     it "returns the information about founded book" do
-  #       book_response = json_response
-  #       expect(book_response[:title]).to eql @book.title
-  #     end
-  #
-  #     it { should respond_with 200 }
-  #   end
-  #
-  #
-  #   context "when book doesn't exist" do
-  #     before(:each) do
-  #       @book = FactoryGirl.create :book
-  #       @book.isbn = "9788373378124"
-  #       get :found_by_isbn, isbn: @book.isbn
-  #     end
-  #
-  #     it { should respond_with 404 }
-  #   end
-  # end
+  describe "GET #found_by_isbn" do
+    context "when is successfully founded" do
+      before(:each) do
+        @book = FactoryGirl.create :book
+        get :found_by_isbn, isbn: @book.isbn
+      end
+
+      it "returns the information about founded book" do
+        book_response = json_response
+        expect(book_response[:title]).to eql @book.title
+      end
+
+      it { should respond_with 200 }
+    end
+
+
+    context "when book doesn't exist" do
+      before(:each) do
+        @book = FactoryGirl.create :book
+        @book.isbn = "9788373378124"
+        get :found_by_isbn, isbn: @book.isbn
+      end
+
+      it { should respond_with 404 }
+    end
+  end
 end
