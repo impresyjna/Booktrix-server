85df0100e48708d85cf6ad799f6b05b6e25479e0
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-05-13 00:14:55 +0200

    Broken build fixed

--- spec/controllers/api/v1/borrow_histories_controller_spec.rb
+++ spec/controllers/api/v1/borrow_histories_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Api::V1::BorrowHistoriesController, type: :controller do
       @borrow_history = BorrowHistory.create(user_book_id: @user_book.id, user_id: @friend.id, borrow_history_state_id: 1)
       get :index, user_book_id: @user_book.id
       borrow_history_response = json_response
-      expect(borrow_history_response).to have_key(:borrow_history)
+      expect(borrow_history_response).to have_key(:borrow_histories)
     end
     it { should respond_with 200 }
   end
