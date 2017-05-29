f1fa5305b0ffb2ba066deab04fbfe76c422ca86c
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-05-30 00:27:34 +0200

    Categories serializer field added

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,8 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :color, :font_color
+  attributes :id, :name, :color, :font_color, :user_books_count
+
+  def user_books_count
+    object.user_books.count
+  end
+
 end
